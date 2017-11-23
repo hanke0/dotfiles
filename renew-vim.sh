e12256e77e70eb6e967beeed0f2b3ac8d54cb34a
ko-han <hanke0@outlook.com>
2017-11-23 22:25:20 +0800

    add commad mv force

--- renew-vim.sh
+++ renew-vim.sh
@@ -3,5 +3,5 @@
 cd ~ && mkdir ~/tmp_111
 git clone  https://github.com/ko-han/.vim.git ~/tmp_111
 rm -rf ~/tmp_111/.git
-mv ~/tmp_111/* ~ && rmdir tmp_111
+mv -f ~/tmp_111/* ~ && rmdir tmp_111
 echo "Great, All Things Hava Done, Enjoy It!"
