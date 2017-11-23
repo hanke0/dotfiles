3dcabbff7843d040266ea9e327f257d7ce8e87e5
ko-han <hanke0@outlook.com>
2017-11-23 22:59:35 +0800

    update renew

--- renew-vim.sh
+++ renew-vim.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
 cd ~ && mkdir ~/tmp_111
-git clone  https://github.com/ko-han/.vim.git ~/tmp_111
-rm -rf ~/tmp_111/.git
-mv -f ~/tmp_111/* ~/ && rmdir tmp_111
+git clone  https://github.com/ko-han/.vim.git ~/tmp_111 || { echo "command failed"; exit 1; }
+rm -rf ~/tmp_111/.git  || { echo "command failed"; exit 1; }
+mv -f ~/tmp_111/* ~/  || { echo "command failed"; exit 1; }
+chmod +x renew-vim.sh || { echo "command failed"; exit 1; }
+rm -rf tmp_111 || { echo "command failed"; exit 1; }
 echo "Great, All Things Hava Done, Enjoy It!"
