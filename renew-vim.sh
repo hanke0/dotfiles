d1d07e8f27e9c0050dc6dc574a173fe1e6934459
ko-han <hanke0@outlook.com>
2017-11-23 23:25:17 +0800

    mv -f change cp -r

--- renew-vim.sh
+++ renew-vim.sh
@@ -3,7 +3,7 @@
 cd ~ && mkdir ~/tmp_111
 git clone  https://github.com/ko-han/.vim.git ~/tmp_111
 rm -rf ~/tmp_111/.git
-mv -fv ~/tmp_111/*  ~/tmp_111/.[^.]* ~/
+cp -r ~/tmp_111/*  ~/tmp_111/.[^.]* ~/
 chmod +x ~/renew-vim.sh
 rm -rf ~/tmp_111
 echo "Great, All Things Hava Done, Enjoy It!"
