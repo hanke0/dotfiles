2a154c576860586fdf5696276c6ebb6e5bcd0d61
ko-han <hanke0@outlook.com>
2017-12-09 18:35:50 +0800

    fix privilege“

--- bin/renew-vim.sh
+++ bin/renew-vim.sh
@@ -1,9 +1,10 @@
-#! /bin/bash
+#!/bin/bash
 
 cd ~ && mkdir ~/tmp_111
-git clone  https://github.com/ko-han/.vim.git ~/tmp_111
-rm -rf ~/tmp_111/.git
-cp -r ~/tmp_111/*  ~/tmp_111/.[^.]* ~/
-chmod +x ~/renew-vim.sh
-rm -rf ~/tmp_111
+git clone  https://github.com/ko-han/.vim.git ~/tmp_han_vim_config
+rm -rf ~/tmp_han_vim_config/.git
+cp -r ~/tmp_111/*  ~/tmp_han_vim_config/.[^.]* ~/
+chmod +x ~/bin/renew-vim.sh
+rm -rf ~/tmp_han_vim_config
+echo ""
 echo "Great, All Things Hava Done, Enjoy It!"
