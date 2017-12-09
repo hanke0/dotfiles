b2740fbb085234aaad3128760d7d925753cd6d9e
ko-han <hanke0@outlook.com>
2017-12-09 18:37:47 +0800

    fix dir

--- bin/renew-vim.sh
+++ bin/renew-vim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~ && mkdir ~/tmp_111
+cd ~ && mkdir ~/tmp_han_vim_config
 git clone  https://github.com/ko-han/.vim.git ~/tmp_han_vim_config
 rm -rf ~/tmp_han_vim_config/.git
 cp -r ~/tmp_111/*  ~/tmp_han_vim_config/.[^.]* ~/
