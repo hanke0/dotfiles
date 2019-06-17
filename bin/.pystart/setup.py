5c2a1729e8eb016ee1146f17db2df33bceaed693
han.ke <han.ke@ricequant.com>
2019-06-17 13:50:03 +0800

    use '-' in package

--- bin/.pystart/setup.py
+++ bin/.pystart/setup.py
@@ -4,14 +4,16 @@ import re
 from setuptools import setup, find_packages
 
 package_name = "example"
+PACKAGE = package_name.replace("_", "-")
+PACKAGE_NAME = package_name.replace("-", "_")
 
-with open(os.path.join(package_name.replace('-', '_'), "__init__.py"), "rt") as f:
+VERSION_REGEX = r"""__version__ ?= ?["'](?P<version>.+?)["']\s*$"""
+
+with open(os.path.join(PACKAGE_NAME, "__init__.py"), "rt") as f:
     value = f.read()
-    match = re.search(r"""__version__ ?= ?["'](?P<version>.+?)["']\s*$""", value, re.MULTILINE)
+    match = re.search(VERSION_REGEX, value, re.MULTILINE)
     version = match.groupdict()["version"]
 
 setup(
-    name=package_name,
-    version=version,
-    packages=find_packages(exclude=["tests", "test.*"]),
+    name=PACKAGE, version=version, packages=find_packages(exclude=["tests", "test.*"])
 )
