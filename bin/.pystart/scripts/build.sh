0143956c9f3ed46e4679651ae2d1d28358a68bb7
han.ke <han.ke@ricequant.com>
2019-08-22 15:38:03 +0800

    auto build docker image

--- bin/.pystart/scripts/build.sh
+++ bin/.pystart/scripts/build.sh
@@ -1,46 +1,92 @@
 #!/usr/bin/env bash
-
 set -e
 
-build() {
-    python setup.py ${PYBUILD_FLAG:=sdist}
+PACKAGE=example
+HERE=$(dirname "$0")
+PROJECT_ROOT=$(dirname ${HERE})
+MODULE=${PACKAGE//-/_}
+MODULE_UPPER=$(echo ${MODULE} | tr '[:lower:]' '[:upper:]')
+CONFIG=${MODULE_UPPER}_CONFIG
+
+VERSION_FILE=${PROJECT_ROOT}/${MODULE}/__init__.py
+VERSION_REGEX='__version__ = "(.*)"'
+VERSION_TEMP_FILE="${VERSION_FILE}abc"
+export TWINE_REPOSITORY_URL=
+export TWINE_USERNAME=
+export TWINE_PASSWORD=
+
+striking-print() {
+    local s="$*"
+    local n=$((${#s} + 6))
+    echo
+    printf '%*s' ${n} '' | tr ' ' '*' && printf '\n'
+    echo '* ' ${s} ' *'
+    printf '%*s' ${n} '' | tr ' ' '*' && printf '\n'
+    echo
 }
 
-function soft-clean {
-	find . -name *.egg-info -exec rm -rf {} +
-	find . -name '*.pyc' -exec rm -f {} +
-	find . -name '*.pyo' -exec rm -f {} +
-	find . -name '*~' -exec rm -f {} +
+if [[ $(grep -E -o "${VERSION_REGEX}" "${VERSION_FILE}") =~ ${VERSION_REGEX} ]]; then
+    VERSION_BASE=${BASH_REMATCH[1]}
+else
+    >&2 echo "can't found version"
+    exit 1
+fi
+
+VERSION_SUFFIX="a0"
+GIT_TAG=$(git log --pretty=%h -1)
+VERSION="${VERSION_BASE}${VERSION_SUFFIX}+${GIT_TAG}"
+
+DOCKER_NAME=${PACKAGE}
+DOCKER_FILE_PATH=${PROJECT_ROOT}
+DOCKER_HUB=hub.docker.com
+DOCKER_TAG="${DOCKER_HUB}/${DOCKER_NAME}:${VERSION_BASE}${VERSION_SUFFIX}.${GIT_TAG}"
+
+update-version() {
+    sed -iabc -e s/__version__\ =.*/__version__\ =\ \"${VERSION}\"/ ${VERSION_FILE}
+    rm -f ${VERSION_TEMP_FILE}
 }
 
-trap soft-clean EXIT
+build-package() {
+    python setup.py -q sdist
+}
 
-clean() {
+upload-package() {
+    python -m twine upload dist/*
+}
+
+soft-clean() {
+    find . -name *.egg-info -exec rm -rf {} +
+    find . -name '*.pyc' -exec rm -f {} +
+    find . -name '*.pyo' -exec rm -f {} +
+    find . -name '*~' -exec rm -f {} +
+}
+
+exit-clean() {
     soft-clean
-	rm -rf dist build
+    git checkout -- ${VERSION_FILE} || true
+    rm -f ${VERSION_TEMP_FILE}
+    >&2 echo "Goodbye!!!"
 }
 
-upload() {
-	twine upload dist/* -r ${PYPI:=rq}
+dist-clean() {
+    soft-clean
+    rm -rf dist build
 }
 
-case $1 in
-    build)
-        clean
-        build
-        soft-clean
-        ;;
-    release)
-        clean
-        build
-        upload
-        soft-clean
-        ;;
-    clean)
-        clean
-        ;;
-    *)
-        2>&1 echo Error
-        exit 1
-        ;;
-esac
+build-docker-image() {
+    docker build --build-arg PACKAGE_VERSION=${VERSION} -t "${DOCKER_TAG}" "${DOCKER_FILE_PATH}"
+}
+
+push-docker-image() {
+    docker push "${DOCKER_TAG}"
+}
+
+trap exit-clean EXIT
+dist-clean
+update-version
+build-package
+striking-print "Package:" $(ls ${PROJECT_ROOT}/etc)
+upload-package
+build-docker-image
+striking-print ""
+push-docker-image
