c8ece7bf46a82a18d21fe4581643b17e65882f7e
ko-han <hanke0@outlook.com>
2019-10-07 23:33:41 +0800

    More useful tools

--- bin/lib.sh
+++ bin/lib.sh
@@ -56,7 +56,7 @@ str_match() {
 
 str_ltrim() {
   local str=${1:-}
-  if (( $# < 2 )); then
+  if (($# < 2)); then
     printf '%s\n' "${str#"${str%%[![:space:]]*}"}"
   else
     printf '%s\n' "${str##$2}"
@@ -65,7 +65,7 @@ str_ltrim() {
 
 str_rtrim() {
   local str=${1:-}
-  if (( $# < 2 )); then
+  if (($# < 2)); then
     printf '%s\n' "${str%"${str##*[![:space:]]}"}"
   else
     printf '%s\n' "${str%%$2}"
@@ -134,6 +134,16 @@ unique_array_add() {
   eval "${array_name}+=(\"$item\" )"
 }
 
+array_unique() {
+  declare -A tmp_array
+
+  for i in "$@"; do
+    [[ $i ]] && IFS=" " tmp_array["${i:- }"]=1
+  done
+
+  printf '%s\n' "${!tmp_array[@]}"
+}
+
 #--------------------------
 #  ACTION
 #--------------------------
@@ -168,6 +178,28 @@ askyes() {
       continue
     fi
   done
+}
+
+get_term_size() {
+  # Usage: get_term_size
+
+  # (:;:) is a micro sleep to ensure the variables are
+  # exported immediately.
+  shopt -s checkwinsize; (:;:)
+  printf '%s\n' "$LINES $COLUMNS"
+}
+
+bar() {
+  # Usage: bar 1 10
+  #            ^----- Elapsed Percentage (0-100).
+  #               ^-- Total length in chars.
+  ((elapsed = $1 * $2 / 100))
+
+  # Create the bar with spaces.
+  printf -v prog "%${elapsed}s"
+  printf -v total "%$(($2 - elapsed))s"
+
+  printf '%s\r' "[${prog// /-}${total}]"
 }
 
 # ONLY BASH >= 4
