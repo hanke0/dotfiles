6c2c14b1f7335acfca7b3633aa1e48b79ffb5e83
ko-han <hanke0@outlook.com>
2019-10-07 21:12:15 +0800

    A example of getopt usage

---
+++ bin/argsparser.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+RAW_PARAMS=("$@")
+
+OPTIONS=di:r:v
+LONGOPTIONS=debug,inputdir:,random:,verbose
+
+PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTIONS --name "$0" -- "$@")
+
+[[ $? -ne 0 ]] && exit 3
+
+eval set -- "$PARSED"
+
+while true; do
+  [[ -z "$1" ]] && break
+  case "$1" in
+  -d | --debug)
+    echo "debug" "$1"
+    shift
+    ;;
+  -i | --inputdir)
+    echo "inputdir" "$1" "$2"
+    shift 2
+    ;;
+  -v | --verbose)
+    echo "verbose $1"
+    shift
+    ;;
+  -r | --random)
+    echo "random" "$1" "$2"
+    shift 2
+    ;;
+  --)
+    shift
+    break
+    ;;
+  *)
+    echo "arguments error $1"
+    ;;
+  esac
+done
+
+echo "unknown: " "$@"
+echo "raw:" "${RAW_PARAMS[@]}"
