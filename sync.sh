78d434379b7ece0a835afd87bbce4b91efb7a791
han.ke <han.ke@ricequant.com>
2020-06-04 11:44:51 +0800

    improve sync.sh

--- sync.sh
+++ sync.sh
@@ -1,46 +1,55 @@
 #!/usr/bin/env bash
 
-__setup_han_dotfiles() {
-    local dryrun="$1"
-    local backupdir=~/.dotfiles.backup
+__han_dotfiles=(
+  .bashrc
+  .condarc
+  .gitconfig
+  .gitignore
+  .inputrc
+  .tmux.conf
+  .vimrc
+  .zshrc
+  bin
+)
 
-    local files=(
-        .bashrc
-        .condarc
-        .gitconfig
-        .gitignore
-        .inputrc
-        .tmux.conf
-        .vimrc
-        .zshrc
-        bin
-    )
-    local file
-    if [[ ! -d "$backupdir" ]]; then
-        $dryrun mkdir -p "$backupdir"
+__setup_han_dotfiles() {
+  local dryrun="$1"
+  local backupdir=~/.dotfiles.backup
+
+  local file
+  if [[ ! -d "$backupdir" ]]; then
+    ${dryrun} mkdir -p "$backupdir"
+  fi
+  for file in "${__han_dotfiles[@]}"; do
+    if [[ -L ~/"$file" ]]; then
+      ${dryrun} unlink ~/"$file"
+    else
+      if [[ -e ~/"$file" ]]; then
+        ${dryrun} cp -rf ~/"$file" "$backupdir/$file"
+      fi
     fi
-    for file in "${files[@]}"; do
-        if [[ -L ~/"$file" ]]; then
-            $dryrun unlink ~/"$file"
-        else
-            if [[ -e ~/"$file" ]]; then
-                $dryrun cp -rf ~/"$file" "$backupdir/$file"
-            fi
-        fi
-        $dryrun cp -rf "$file" ~/
-    done
-    $dryrun . ~/.bashrc
+    ${dryrun} cp -rf "$file" ~/
+  done
+  ${dryrun} . ~/.bashrc
 }
 
 case "$1" in
 -n | --dry-run | n | dry | dry-run)
-    __setup_han_dotfiles "echo"
-    ;;
+  __setup_han_dotfiles "echo"
+  ;;
 -h | --help | help)
-    echo "$0" "[-n --dry-run] [-h --help]"
-    ;;
+  echo "$0" "[-n --dry-run] [-h --help]"
+  ;;
 *)
+  echo -n "Do you wish to overwrite ${__han_dotfiles[*]}?[y/N] "
+  read -r answer
+  case ${answer} in
+  [Yy]*)
     __setup_han_dotfiles
     ;;
+  *) ;;
+  esac
+  ;;
 esac
 unset __setup_han_dotfiles
+unset __han_dotfiles
