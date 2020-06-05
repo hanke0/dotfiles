591cc1deab8dc1ecf2ae335b266825acc203e16e
han.ke <han.ke@ricequant.com>
2020-06-05 14:52:10 +0800

    fix

--- bin/argsparser.sh
+++ bin/argsparser.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-RAW_PARAMS=("$@")
+RAW_PARAMS=($@)
 
 OPTIONS=di:r:v
 LONGOPTIONS=debug,inputdir:,random:,verbose
@@ -11,32 +11,32 @@ PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTIONS --name "$0" -- "$@
 eval set -- "$PARSED"
 
 while true; do
-    [[ -z "$1" ]] && break
-    case "$1" in
-    -d | --debug)
-        echo "debug" "$1"
-        shift
-        ;;
-    -i | --inputdir)
-        echo "inputdir" "$1" "$2"
-        shift 2
-        ;;
-    -v | --verbose)
-        echo "verbose $1"
-        shift
-        ;;
-    -r | --random)
-        echo "random" "$1" "$2"
-        shift 2
-        ;;
-    --)
-        shift
-        break
-        ;;
-    *)
-        echo "arguments error $1"
-        ;;
-    esac
+  [[ -z "$1" ]] && break
+  case "$1" in
+  -d | --debug)
+    echo "debug" "$1"
+    shift
+    ;;
+  -i | --inputdir)
+    echo "inputdir" "$1" "$2"
+    shift 2
+    ;;
+  -v | --verbose)
+    echo "verbose $1"
+    shift
+    ;;
+  -r | --random)
+    echo "random" "$1" "$2"
+    shift 2
+    ;;
+  --)
+    shift
+    break
+    ;;
+  *)
+    echo "arguments error $1"
+    ;;
+  esac
 done
 
 echo "unknown: " "$@"
--- bin/rsslimit.sh
+++ bin/rsslimit.sh
@@ -3,24 +3,24 @@
 set -e
 
 RssOf() {
-    grep ^Rss /proc/$1/smaps | awk '{sum += $2} END {print sum}'
+	grep ^Rss /proc/"$1"/smaps | awk '{sum += $2} END {print sum}'
 }
 
 usage() {
-    echo 'Kill process if it reach max limit.'
-    echo
-    echo 'Usage:'
-    echo " $0 [options] RssLimit[KB] PID"
-    echo
-    echo 'Options:'
-    echo '    -s    Interval in seconds, positive number'
-    echo '    -q    Quiet'
+	echo 'Kill process if it reach max limit.'
+	echo
+	echo 'Usage:'
+	echo " $0 [options] RssLimit[KB] PID"
+	echo
+	echo 'Options:'
+	echo '    -s    Interval in seconds, positive number'
+	echo '    -q    Quiet'
 }
 
 wrong-usage() {
-    echo 'Wrong Usage' $1
-    usage
-    exit 2
+	echo 'Wrong Usage' "$1"
+	usage
+	exit 2
 }
 
 quiet=0
@@ -29,28 +29,28 @@ interval=1
 IsNum='^[0-9]+$'
 
 while getopts s:qm:h opt; do
-    case ${opt} in
-    s)
-        interval=$OPTARG
-        ;;
-    q)
-        quiet=1
-        ;;
-    m)
-        MAX=$OPTARG
-        ;;
-    h)
-        usage
-        exit 0
-        ;;
-    : | ?)
-        usage
-        exit 1
-        ;;
-    esac
+	case ${opt} in
+	s)
+		interval=$OPTARG
+		;;
+	q)
+		quiet=1
+		;;
+	m)
+		MAX=$OPTARG
+		;;
+	h)
+		usage
+		exit 0
+		;;
+	: | ?)
+		usage
+		exit 1
+		;;
+	esac
 done
 
-shift $(($OPTIND - 1))
+shift $((OPTIND - 1))
 
 [[ $# -ne 2 ]] && wrong-usage
 
@@ -63,15 +63,15 @@ shift $(($OPTIND - 1))
 
 [[ ! -e /proc/${PID}/smaps ]] && echo >&2 PID not exists && exit 1
 
-echo Listen PID ${PID} RssLimit ${MAX} KB
+echo Listen PID "${PID}" RssLimit "${MAX}" KB
 
 while :; do
-    _rss=$(RssOf ${PID})
-    [[ ${quiet} -eq 0 ]] && echo >&2 USE ${_rss} kb
-    if [[ ${_rss} -gt ${MAX} ]]; then
-        echo >&2 Overflow, kill PID ${PID}
-        kill ${PID}
-        exit 1
-    fi
-    sleep ${interval}
+	_rss=$(RssOf "${PID}")
+	[[ ${quiet} -eq 0 ]] && echo >&2 USE ${_rss} kb
+	if [[ ${_rss} -gt ${MAX} ]]; then
+		echo >&2 Overflow, kill PID "${PID}"
+		kill "${PID}"
+		exit 1
+	fi
+	sleep "${interval}"
 done
