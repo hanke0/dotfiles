0a00bd246cdb93a8eb21fe71b9d4c3b1527e1381
ko-han <hanke0@outlook.com>
2021-04-02 12:11:54 +0800

    Fix sync.sh always copy file

--- sync.sh
+++ sync.sh
@@ -1,72 +1,76 @@
 #!/usr/bin/env bash
 
 __han_dotfiles=(
-  .bashrc
-  .condarc
-  .gitconfig
-  .gitignore
-  .inputrc
-  .tmux.conf
-  .vimrc
-  .zshrc
-  bin
+    .bashrc
+    .condarc
+    .gitconfig
+    .gitignore
+    .inputrc
+    .tmux.conf
+    .vimrc
+    .zshrc
+    bin
 )
 
 __han_dotfiles_ask=false
 
 __ask_user_permit() {
-  if [[ $__han_dotfiles_ask = "true" ]]; then
-    return 0
-  fi
-  printf "%s" "Do you wish to overwrite ${1}?[y/N] "
-  local answer
-  read -r answer
-  case ${answer} in
-  [Yy]*)
-    return 0
-    ;;
-  *)
-    return 1
-    ;;
-  esac
+    if [[ $__han_dotfiles_ask = "true" ]]; then
+        return 0
+    fi
+    printf "%s" "Do you wish to overwrite ${1}?[y/N] "
+    local answer
+    read -r answer
+    case ${answer} in
+    [Yy]*)
+        return 0
+        ;;
+    *)
+        return 1
+        ;;
+    esac
 }
 
 __setup_han_dotfiles() {
-  local dryrun="$1"
-  local backupdir=~/.dotfiles.backup
+    local dryrun="$1"
+    local backupdir=~/.dotfiles.backup/$(date +%Y-%m-%dT%H-%M-%S)
 
-  local file
-  if [[ ! -d "$backupdir" ]]; then
-    ${dryrun} mkdir -p "$backupdir"
-  fi
-  for file in "${__han_dotfiles[@]}"; do
-    if [[ -L ~/"$file" ]]; then
-      ${dryrun} unlink ~/"$file"
+    local file
+    if [[ ! -d "$backupdir" ]]; then
+        ${dryrun} mkdir -p "$backupdir"
     fi
-    if [[ -e ~/"$file" ]]; then
-      if __ask_user_permit "$file"; then
-        ${dryrun} cp -rf ~/"$file" "$backupdir/$file"
-      fi
-    fi
-    ${dryrun} cp -rf "$file" ~/
-  done
-  ${dryrun} . ~/.bashrc
+    for file in "${__han_dotfiles[@]}"; do
+        if [[ -L ~/"$file" ]]; then
+            ${dryrun} unlink ~/"$file"
+        fi
+        if __ask_user_permit "$file"; then
+            if [[ -e ~/"$file" ]]; then
+                ${dryrun} cp -rf ~/"$file" "$backupdir/$file"
+            fi
+            ${dryrun} cp -rf "$file" ~/
+        fi
+    done
+    ${dryrun} . ~/.bashrc
 }
 
 case "$1" in
--n | --dry-run | n | dry | dry-run)
-  __setup_han_dotfiles "echo"
-  ;;
--h | --help | help)
-  echo "$0" "[-n --dry-run] [-h --help]"
-  ;;
--y | --yes)
-  __han_dotfiles_ask=true
-  __setup_han_dotfiles
-  ;;
+-n)
+    __setup_han_dotfiles "echo"
+    ;;
+-h)
+    echo "$0" "[-n] [-h] [-ny]"
+    ;;
+-y)
+    __han_dotfiles_ask=true
+    __setup_han_dotfiles
+    ;;
+-ny)
+    __han_dotfiles_ask=true
+    __setup_han_dotfiles "echo"
+    ;;
 *)
-  __setup_han_dotfiles
-  ;;
+    __setup_han_dotfiles
+    ;;
 esac
 
 unset __setup_han_dotfiles
