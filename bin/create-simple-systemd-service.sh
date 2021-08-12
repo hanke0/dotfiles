3a535893ef6eedfd6acf2fb529a4a75a3aa7bd98
ko-han <hanke0@outlook.com>
2021-08-12 16:39:05 +0800

    Add a script to create simple systemd service

---
+++ bin/create-simple-systemd-service.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+DESCRIPTION=""
+USER=""
+EXEC=""
+SERVICE_NAME=""
+DIR=""
+read -r -p "Executeable: " EXEC
+if [[ -z "$EXEC" ]]; then
+    EXEC="$@"
+    if [[ -z "$EXEC" ]]; then
+        echo >&2 "please input the executable empty"
+        exit 1
+    fi
+fi
+
+read -r -p "Working Directory: " DIR
+if [[ -z "$DIR" ]]; then
+    DIR=/tmp
+fi
+
+getbase() {
+    echo $* | sed -E 's/[[:blank:]].*//g' | xargs basename
+}
+
+read -r -p "User: " USER
+if [[ -z "$USER" ]]; then
+    USER=$(whoami)
+fi
+
+read -r -p "Service name: " SERVICE_NAME
+if [[ -z "$SERVICE_NAME" ]]; then
+    SERVICE_NAME=$(getbase $EXEC)
+fi
+
+read -r -p "Description: " DESCRIPTION
+if [[ -z "$DESCRIPTION" ]]; then
+    DESCRIPTION=$FILENAME
+fi
+
+FILE_CONTENT="
+[Unit]
+Description=$DESCRIPTION
+After=network.target
+StartLimitIntervalSec=0
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+StartLimitInterval=60s
+User=$USER
+ExecStart=$EXEC
+WorkingDirectory=$DIR
+[Install]
+WantedBy=multi-user.target
+"
+
+TMP_FILE="/tmp/systemd-$SERVICE_NAME-$RANDOM.service"
+printf "$FILE_CONTENT" >"$TMP_FILE"
+echo
+cat "$TMP_FILE"
+DST_FILE="/etc/systemd/system/$SERVICE_NAME.service"
+ANSWER=
+read -r -p "Above content will write to $DST_FILE, OK?[Y/n]: " ANSWER
+if [[ -n "$ANSWER" && "$ANSWER" != "y" ]]; then
+    echo "User abort"
+    exit 127
+fi
+ANSWER=
+
+set -e
+sudo cp "$TMP_FILE" "$DST_FILE"
+sudo systemctl start "$SERVICE_NAME"
+read -r -p "Enable start on boot? [y/N]: " ANSWER
+if [[ "$ANSWER" == "y" ]]; then
+    sudo systemctl enable "$SERVICE_NAME"
+fi
+systemctl status "$SERVICE_NAME"
