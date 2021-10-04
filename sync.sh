a9e26f7bed599f2f9a89cd63fd2bbda57c9eb778
ko-han <hanke0@outlook.com>
2021-10-04 13:56:23 +0800

    Source config, not rewrite config file

--- sync.sh
+++ sync.sh
@@ -1,79 +1,64 @@
 #!/usr/bin/env bash
+set -e
 
-__han_dotfiles=(
-    .bashrc
-    .condarc
-    .gitconfig
-    .gitignore
-    .inputrc
-    .tmux.conf
-    .vimrc
-    .zshrc
-    bin
-)
+ABS_PATH="$(realpath "$0")"
+ROOT_DIR="$(dirname "$ABS_PATH")"
 
-__han_dotfiles_ask=false
-
-__ask_user_permit() {
-    if [[ $__han_dotfiles_ask = "true" ]]; then
-        return 0
-    fi
-    printf "%s" "Do you wish to overwrite ${1}?[y/N] "
-    local answer
-    read -r answer
-    case ${answer} in
-    [Yy]*)
-        return 0
-        ;;
-    *)
+_has_content() {
+    if [[ ! -f $2 ]]; then
         return 1
-        ;;
-    esac
-}
-
-__setup_han_dotfiles() {
-    local dryrun="$1"
-    local backupdir=~/.dotfiles.backup/$(date +%Y-%m-%dT%H-%M-%S)
-
-    local file
-    if [[ ! -d "$backupdir" ]]; then
-        ${dryrun} mkdir -p "$backupdir"
     fi
-    for file in "${__han_dotfiles[@]}"; do
-        if [[ -L ~/"$file" ]]; then
-            ${dryrun} unlink ~/"$file"
-        fi
-        if __ask_user_permit "$file"; then
-            if [[ -e ~/"$file" ]]; then
-                ${dryrun} cp -rf ~/"$file" "$backupdir/$file"
-            fi
-            ${dryrun} cp -rf "$file" ~/
-        fi
-    done
-    ${dryrun} . ~/.bashrc
+    grep "$1" "$2" 2>&1 >/dev/null
 }
 
-case "$1" in
--n)
-    __setup_han_dotfiles "echo"
-    ;;
--h)
-    echo "$0" "[-n] [-h] [-ny]"
-    ;;
--y)
-    __han_dotfiles_ask=true
-    __setup_han_dotfiles
-    ;;
--ny)
-    __han_dotfiles_ask=true
-    __setup_han_dotfiles "echo"
-    ;;
-*)
-    __setup_han_dotfiles
-    ;;
-esac
+_put_content() {
+    if _has_content "$1" "$2"; then
+        return 0
+    fi
+    echo "modified $2"
+    echo "$1" >>"$2"
+}
 
-unset __setup_han_dotfiles
-unset __han_dotfiles
-unset __ask_user_permit
-unset __han_dotfiles_ask
+# bashrc config
+_put_content "export PAHT=\"\$PATH:$ROOT_DIR/bin\" # modified by han" ~/.bashrc
+_put_content "[[ -f '$ROOT_DIR/.bashrc' ]] && . '$ROOT_DIR/.bashrc'  # modified by han" ~/.bashrc
+
+# git config
+git config --global include.path "$ROOT_DIR/.gitconfig"
+
+# tmux config
+_put_content "source-file $ROOT_DIR/.tmux.conf  # modified by han" ~/.tmux.conf
+
+# vim config
+_put_content "source $ROOT_DIR/.vimrc \" modified by han" ~/.vimrc
+
+# zsh config
+_put_content "[[ -f '$ROOT_DIR/.zshrc' ]] && . '$ROOT_DIR/.zshrc'  # modified by han" ~/.zshrc
+
+# input config
+_put_content "\$include $ROOT_DIR/.inputrc # modified by han" ~/.inputrc
+
+# cronjob auto update
+CRON_JOB="su -s /bin/sh nobody -c 'cd $ROOT_DIR && /usr/bin/git pull -q origin master'"
+if type crontab 2>&1 >/dev/null; then
+    if crontab -l | grep "$CRON_JOB" 2>&1 >/dev/null; then
+        echo "already has cron job"
+    else
+        (
+            crontab -l
+            printf "* * * * * $CRON_JOB\r\n"
+        ) | crontab -
+    fi
+fi
+
+cat <<EOF
+Success setup! All configuration will active in next login.
+
+# Optional configuration
+
+# git ignore
+cp -f $ROOT_DIR/.gitignore ~/.gitignore
+
+# condarc
+cp -f $ROOT_DIR/.condarc ~/.condarc
+EOF
