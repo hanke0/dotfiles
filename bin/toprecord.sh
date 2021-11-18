94625abafc8a4b899a8ec272c6040e6d22665f79
ko-han <hanke0@outlook.com>
2021-11-18 22:58:47 +0800

    Add a script to record process memory and cpu usage

---
+++ bin/toprecord.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+case "$1" in
+-h | \? | --help | "")
+    echo "Usage: $0 pattern"
+    echo "This script will output the process memory and cpu usage repeatly."
+    echo "the output has following format: date time pid RES %CPU"
+    exit 127
+    ;;
+*) ;;
+esac
+
+top -b -d1 | grep --line-buffered -E "$1" |
+    grep --line-buffered -v "grep --line-buffered -E $1" |
+    grep -v --line-buffered "$0" |
+    awk '{print strftime("%Y-%m-%e %H:%M:%S"),$1,$6,$9}'
