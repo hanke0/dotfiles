a8fdfb367bc0b528bca7493a5627743c1b0a51dc
ko-han <hanke0@outlook.com>
2021-11-23 15:15:16 +0800

    Add env in cron job script

--- update.sh
+++ update.sh
@@ -2,6 +2,21 @@
 
 set -e
 
+path_append() {
+    case ":${PATH}:" in
+    *:"$1":*) ;;
+    *)
+        export PATH=$PATH:$1
+        ;;
+    esac
+}
+
+path_append "/usr/local/bin"
+path_append "/bin"
+path_append "/usr/bin"
+path_append "/sbin"
+path_append "/usr/sbin"
+
 ABS_PATH="$(realpath "$0")"
 ROOT_DIR="$(dirname "$ABS_PATH")"
 
