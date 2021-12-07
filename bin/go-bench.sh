c4ca2853834e189f128b3a18fa7763fa2b55ba45
ko-han <hanke0@outlook.com>
2021-12-07 14:58:13 +0800

    Fix go-bench.sh flag not support error

--- bin/go-bench.sh
+++ bin/go-bench.sh
@@ -15,8 +15,8 @@ vexec() {
 f="$(mktemp)"
 
 vexec go clean -testcache
-vexec go test -gcflags=all=-l --bench=. --run=^$ \
-    -benchmem --benchmark_out "$f" \
+vexec go test -gcflags=all=-l --bench=".*" --run=^$ \
+    -benchmem \
     "${args[@]}"
 
 cat "$f"
