b495ce7cf4815601ca3b7c0a765ee19682431cc3
ko-han <hanke0@outlook.com>
2021-12-07 14:28:30 +0800

    Set default coverpkg to local package in go-cover.sh

--- bin/go-cover.sh
+++ bin/go-cover.sh
@@ -4,14 +4,14 @@ args=()
 if [[ $# -gt 0 ]]; then
     args=("$@")
 else
-    args=(./...)
+    args=(-coverpkg=./... ./...)
 fi
 
-run_and_print() {
+vexec() {
     echo "$@"
     "$@"
 }
 
-run_and_print rm -f cover.out cover.html
-run_and_print go test -gcflags=all=-l -covermode=count -coverprofile=cover.out "${args[@]}"
-run_and_print go tool cover -html=cover.out -o cover.html
+vexec rm -f cover.out cover.html
+vexec go test -gcflags=all=-l -covermode=count -coverprofile=cover.out "${args[@]}"
+vexec go tool cover -html=cover.out -o cover.html
