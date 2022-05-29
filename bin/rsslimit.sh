b32213b9c117cb3ec68e06b775f3e86671de762a
ko-han <hanke0@outlook.com>
2022-05-30 00:59:48 +0800

    Update rsslimit.sh

--- bin/rsslimit.sh
+++ bin/rsslimit.sh
@@ -7,14 +7,16 @@ RssOf() {
 }
 
 usage() {
-    echo 'Kill process if it reach max limit.'
-    echo
-    echo 'Usage:'
-    echo " $0 [options] RssLimit[KB] PID"
-    echo
-    echo 'Options:'
-    echo '    -s    Interval in seconds, positive number'
-    echo '    -q    Quiet'
+    cat <<EOF
+Usage: ${0##*/} [OPTION] <rss-bytes> <max-kilobytes> 
+Warning process if it reach max limit.
+
+Option:
+    -s    interval in seconds, positive number.
+    -q    quiet output.
+    -k    kill it when reach max limit
+    -n    signal number passed to kill
+EOF
 }
 
 wrong-usage() {
@@ -27,50 +29,67 @@ quiet=0
 interval=1
 
 IsNum='^[0-9]+$'
+PID=
+MAX=
+KILL=0
+KILLARGS=
 
-while getopts s:qm:h opt; do
-    case ${opt} in
-    s)
-        interval=$OPTARG
+while [ $# -gt 0 ]; do
+    case "$1" in
+    -s)
+        interval="$2"
+        shift 2
         ;;
-    q)
+    -q)
         quiet=1
+        shift
         ;;
-    m)
-        MAX=$OPTARG
-        ;;
-    h)
+    -h | --help)
         usage
         exit 0
         ;;
-    : | ?)
-        usage
-        exit 1
+    -k)
+        KILL=1
+        ;;
+    -n)
+        KILLARGS="-n $2"
+        shift 2
+        ;;
+    *)
+        if [ -n "$PID" ]; then
+            if [ -n "$MAX" ]; then
+                wrong-usage "too many pids"
+            else
+                MAX="$1"
+            fi
+        else
+            PID="$1"
+        fi
+        shift
         ;;
     esac
 done
 
-shift $((OPTIND - 1))
-
-[[ $# -ne 2 ]] && wrong-usage
-
-[[ -n $1 ]] && PID=$1
+[ -z "$PID" ] && wrong-usage "must provides one pid"
+[ -z "$MAX" ] && wrong-usage "must provides memory limit"
 
 [[ -z ${MAX} || ! ${MAX} =~ ${IsNum} ]] && wrong-usage 'wrong Rss'
 [[ -z ${PID} || ! ${PID} =~ ${IsNum} ]] && wrong-usage 'wrong PID'
-
 [[ ! ${interval} =~ ${IsNum} ]] && wrong-usage 'wrong interval'
 
 [[ ! -e /proc/${PID}/smaps ]] && echo >&2 PID not exists && exit 1
 
 echo Listen PID "${PID}" RssLimit "${MAX}" KB
 
-while :; do
+while true; do
     _rss=$(RssOf "${PID}")
-    [[ ${quiet} -eq 0 ]] && echo >&2 USE ${_rss} kb
+    [[ ${quiet} -eq 0 ]] && echo >&2 "USE ${_rss} KB"
     if [[ ${_rss} -gt ${MAX} ]]; then
-        echo >&2 Overflow, kill PID "${PID}"
-        kill "${PID}"
+        echo >&2 "Overflow of pid ${PID} RSS: ${_rss}KB"
+        if [ $KILL -ne 0 ]; then
+            echo >&2 "kill $KILLARGS ${PID}"
+            kill $KILLARGS $PID
+        fi
         exit 1
     fi
     sleep "${interval}"
