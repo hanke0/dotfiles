eda33f0bfa1f3c8534a7feae656b60ef4d491beb
hanke <hanke0@outlook.com>
2022-10-13 11:56:41 +0000

    Add a script replace $var to ${var}

---
+++ bin/hibrace.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+usage() {
+    cat <<EOF
+Usage: ${0##*/} [-i] [-h] [input files]...
+Replace all scripts use \$var to \${var}
+
+OPTION:
+    -y                no ask.
+    -h --help         print this help and exit.
+    -i --in-place     edit files in place
+EOF
+}
+
+sedopts=()
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+    -h | --help)
+        usage
+        exit 0
+        ;;
+    -i | --in-place)
+        sedopts+=("${1}")
+        shift
+        ;;
+    --)
+        shift
+        break
+        ;;
+    *)
+        break
+        ;;
+    esac
+done
+
+sed="sed"
+if command -v "gsed"; then
+    sed="gsed"
+fi
+
+"${sed}" "${sedopts[@]}" -E 's/([^\\])\$([a-zA-Z_0-9]+)/\1${\2}/g' "$@"
