30a26e49fb6f8ee651202efcbd1e26ee10fb6586
hanke <hanke0@outlook.com>
2023-03-09 03:42:59 +0000

    Refactoring go test scripts

--- bin/go-test.sh
+++ bin/go-test.sh
@@ -1,23 +1,98 @@
 #!/usr/bin/env bash
 
-args=()
-if [[ $# -gt 0 ]]; then
-    args=("$@")
-else
-    args=(./...)
-fi
+set -e
+set -o pipefail
 
-vexec() {
-    echo "$@"
-    "$@"
+usage() {
+    cat <<EOF
+Usage: ${0##*/} [OPTION]... path
+Run go test easily.
+
+Options:
+    -h, --help              print this text and exit.
+    -c, --coverage          write coverage profile to cover.out and cover.html.
+    -b, --benchmark         run test with benchmark.
+    -r, --recursive         test directory and thire content contains a go.mod recursive.
+    -t, --timeout=Duration  if a test runs longer than this duration, panic.(default to 10 minutes).
+        --cpuprofile        write a CPU profile to cpu.out
+        --memprofile        wirte a alloctaion profile to mem.out.
+EOF
 }
 
-here="$(pwd)"
+declare -a args
 
-find "$here" -name 'go.mod' -type f -exec dirname {} \; | while IFS= read -r folder; do
-    cd "$folder" || exit 1
-    pwd
-    vexec go clean -testcache
-    vexec go test -gcflags=all=-l "${args[@]}"
-    cd "$here" || exit 1
+COVERAGE=false
+RECURSIVE=false
+EXTRA=()
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+    -h | --help)
+        usage
+        exit 0
+        ;;
+    -c | --coverage)
+        shift
+        EXTRA+=(-covermode=atomic -race -coverprofile=cover.out)
+        COVERAGE=true
+        ;;
+    -b | --benchmark)
+        shift
+        EXTRA+=('-run=^$' '-bench=.')
+        ;;
+    -r | --recursive)
+        shift
+        RECURSIVE=true
+        ;;
+    -t | --timeout)
+        EXTRA+=(-timeout "$2")
+        shift
+        ;;
+    --cpuprofile)
+        EXTRA+=("-cpuprofile=cpu.out")
+        shift
+        ;;
+    --memprofile)
+        EXTRA+=("-memprofile=mem.out")
+        shift
+        ;;
+    -*)
+        echo >&2 "unknown option: $1"
+        exit 1
+        ;;
+    --)
+        shift 1
+        args+=("$@")
+        break
+        ;;
+    *)
+        args+=("$1")
+        shift 1
+        ;;
+    esac
 done
+
+doone() {
+    go test -failfast "${EXTRA[@]}" "$path"/...
+}
+
+if [ "${#args[@]}" -eq 0 ]; then
+    args=(".")
+fi
+
+for path in "${args}[@]"; do
+    if [ "$RECURSIVE" = true ]; then
+        find "$here" -print0 -name 'go.mod' -type f -exec dirname {} \; | while read -d '' -r folder; do
+            doone "$folder"
+        done
+        elseclear
+        git s
+        git s
+
+        doone "$path"
+    fi
+done
+
+if [ "$COVERAGE" = true ]; then
+    go tool cover -html=cover.out -o cover.html
+fi
