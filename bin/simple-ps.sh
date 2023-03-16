963c73fd08df6a7a2c696297aa42e275ed9958ab
hanke0 <hanke0@outlook.com>
2023-03-17 02:31:14 +0800

    Add simple-ps.sh

---
+++ bin/simple-ps.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+usage() {
+    cat <<EOF
+Usage: ${0##*/} [OPTION]
+Print all process pid and command.
+
+OPTION:
+    -h --help         print this help and exit.
+EOF
+}
+
+declare -a args
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+    -h | --help)
+        usage
+        exit 0
+        ;;
+    --)
+        shift 1
+        args+=("$@")
+        break
+        ;;
+    -*)
+        echo >&2 "unknown option: $1"
+        exit 1
+        ;;
+    *)
+        args+=("$1")
+        shift 1
+        ;;
+    esac
+done
+
+for pid in /proc/*; do
+    if [ -r "$pid/cmdline" ]; then
+        printf '%-12s %s\n' "${pid##*/}" "$(tr -d '\0' <"${pid}/cmdline")"
+    fi
+done
